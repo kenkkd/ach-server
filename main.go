3319906da0ba28b8842d14517986959dec130eda
Kenta Kakeda <kenta.kakeda@amd-lab.com>
2023-02-26 19:26:54 +0900

    Use mysql config

--- main.go
+++ main.go
@@ -1,26 +1,45 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
+
 	"github.com/kenkkd/ach-server/handler"
+	"github.com/kenkkd/ach-server/pkg/config"
 	"github.com/kenkkd/ach-server/pkg/ent"
 )
 
+func init() {
+	config.Setup()
+}
+
 func main() {
-	client, err := ent.Open("mysql", "root:pass@tcp(localhost:3306)/ach")
+	mysqlConfig := config.ApplicationConfig.MySQL
+	dataSourceName := fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True",
+		mysqlConfig.User, mysqlConfig.Password, mysqlConfig.Host, mysqlConfig.Port, mysqlConfig.Database)
+	client, err := ent.Open("mysql", dataSourceName)
 	if err != nil {
 		log.Fatalf("failed connecting to mysql: %v", err)
 	}
 	log.Println("success connecting to mysql")
 
-	defer client.Close()
+	defer func(client *ent.Client) {
+		err = client.Close()
+		if err != nil {
+			log.Fatalf("failed closing ent client: %v", err)
+		}
+	}(client)
 
 	router := gin.Default()
 
 	handler.HelloWorldHandler(router)
 
-	router.Run()
+	err = router.Run()
+	if err != nil {
+		log.Fatalf("failed running router: %v", err)
+	}
 }
